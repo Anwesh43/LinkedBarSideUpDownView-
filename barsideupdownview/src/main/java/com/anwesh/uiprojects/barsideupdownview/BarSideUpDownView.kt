8bb31672166bd0dd0df301f07ec471c75824e745
Anwesh43 <anweshthecool0@gmail.com>
2020-07-21 15:28:53 +0530

    adding BarSideUpDown class

--- barsideupdownview/src/main/java/com/anwesh/uiprojects/barsideupdownview/BarSideUpDownView.kt
+++ barsideupdownview/src/main/java/com/anwesh/uiprojects/barsideupdownview/BarSideUpDownView.kt
@@ -158,4 +158,27 @@ class BarSideUpDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarSideUpDown(var i : Int) {
+
+        private var curr : UDSBNode = UDSBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
