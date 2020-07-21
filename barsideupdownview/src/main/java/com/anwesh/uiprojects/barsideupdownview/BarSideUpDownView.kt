76de2ea1878dc8e9d8dbf1de4a70d3b57ba7e8b9
Anwesh43 <anweshthecool0@gmail.com>
2020-07-21 15:21:27 +0530

    adding UDSBNode class

--- barsideupdownview/src/main/java/com/anwesh/uiprojects/barsideupdownview/BarSideUpDownView.kt
+++ barsideupdownview/src/main/java/com/anwesh/uiprojects/barsideupdownview/BarSideUpDownView.kt
@@ -117,4 +117,45 @@ class BarSideUpDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class UDSBNode(var i : Int, val state : State = State()) {
+
+        private var next : UDSBNode? = null
+        private var prev : UDSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = UDSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawUDSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : UDSBNode {
+            var curr : UDSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
